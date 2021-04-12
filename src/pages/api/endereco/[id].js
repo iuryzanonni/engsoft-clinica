ed380781e906ddfebd1c36405df317bcd84b6c0e
Iury Zanonni <izanonni@gmail.com>
2021-04-11 23:59:22 -0300

    Criação das rotas da API para insert e get de endereço

---
+++ src/pages/api/endereco/[id].js
@@ -0,0 +1,17 @@
+import database from "../../../database";
+
+async function InsertEndereco(request, response) {
+    let results;
+    try {
+        results = await database("base_de_endereco").where(
+            "cep",
+            request.query.id
+        );
+        console.log(request);
+        return response.send(results);
+    } catch (error) {
+        return response.json({ message: error.message }).send(500);
+    }
+}
+
+export default InsertEndereco;
