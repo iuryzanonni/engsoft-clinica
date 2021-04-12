c7f5ebb631f0714e4c36ce48c9be85bbdc139159
Iury Zanonni <izanonni@gmail.com>
2021-04-12 20:22:37 -0300

    Correção do arquivo knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,11 +2,22 @@ module.exports = {
     development: {
         client: "mysql",
         connection: {
-            host: 'disney-med.ctslaxzrfqjd.us-east-1.rds.amazonaws.com',
+            host: "disney-med.ctslaxzrfqjd.us-east-1.rds.amazonaws.com",
             port: 3306,
-            database: 'DisneyMedical',
-            user: 'admin',
-            password: 'ihRT82wkOi0qhvoZvOqL',
+            database: "DisneyMedical",
+            user: "admin",
+            password: "ihRT82wkOi0qhvoZvOqL",
         },
-    }
-}
+        pool: {
+            min: 0,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+            directory: `${__dirname}/src/database/migrations`,
+        },
+        seeds: {
+            directory: `${__dirname}/src/database/seeds`,
+        },
+    },
+};
