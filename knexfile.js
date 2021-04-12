c86a38da0895e09c410f3cd179f8c1bb70ac980c
Leonardo Leite <42722914+LeonardoLeiteMeira@users.noreply.github.com>
2021-04-12 20:07:27 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,22 +2,11 @@ module.exports = {
     development: {
         client: "mysql",
         connection: {
-            host: process.env.MYSQL_HOST,
-            port: process.env.MYSQL_PORT,
-            database: process.env.MYSQL_DATABASE,
-            user: process.env.MYSQL_USERNAME,
-            password: process.env.MYSQL_PASSWORD,
+            host: 'disney-med.ctslaxzrfqjd.us-east-1.rds.amazonaws.com',
+            port: 3306,
+            database: 'DisneyMedical',
+            user: 'admin',
+            password: 'ihRT82wkOi0qhvoZvOqL',
         },
-        pool: {
-            min: 0,
-            max: 10,
-        },
-        migrations: {
-            tableName: "knex_migrations",
-            directory: `${__dirname}/src/database/migrations`,
-        },
-        seeds: {
-            directory: `${__dirname}/src/database/seeds`,
-        },
-    },
-};
+    }
+}
