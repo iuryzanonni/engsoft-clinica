ee72cafab638ba18ae63bfd9922d62f02c0f4090
Iury Zanonni <izanonni@gmail.com>
2021-04-13 13:52:57 -0300

    Listagem funcionarios

--- src/pages/api/funcionario/index.js
+++ src/pages/api/funcionario/index.js
@@ -3,7 +3,16 @@ import database from "../../../database";
 async function InsertEndereco(request, response) {
     let results;
     try {
-        results = await database.select().from("funcionario");
+        results = await database("pessoa")
+            .join("funcionario", "pessoa.codigo", "funcionario.codigo")
+            .leftJoin("medico", "funcionario.codigo", "medico.codigo")
+            .select(
+                "pessoa.*",
+                "funcionario.data_contrato",
+                "funcionario.salario",
+                "medico.crm",
+                "medico.especialidade"
+            );
         return response.send(results);
     } catch (error) {
         return response.json({ message: error.message }).send(500);
