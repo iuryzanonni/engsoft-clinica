0d0ea628e5632fa66637cc8b2ef4d5d92b3bb4ed
Iury Zanonni <izanonni@gmail.com>
2021-04-13 23:54:05 -0300

    Bug fix

--- src/pages/api/agenda/index.js
+++ src/pages/api/agenda/index.js
@@ -3,7 +3,11 @@ import database from "../../../database";
 async function GetAgenda(request, response) {
     let results;
     try {
-        results = await database.select().from("agenda");
+        results = await database
+            .select()
+            .from("agenda")
+            .join("medico", "agenda.codigomedico", "medico.codigo")
+            .join("pessoa", "pessoa.codigo", "medico.codigo");
         return response.send(results);
     } catch (error) {
         return response.json({ message: error.message }).send(500);
